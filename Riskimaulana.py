6ce6b9a7e98e9381ae3c59477c1ea7548c7df911
RiskiMaulana2 <maulanariski723@gmail.com>
2026-08-26 22:04:53 +0700

    Create Riskimaulana.py

---
+++ Riskimaulana.py
@@ -0,0 +1,6 @@
+print("Riski Maulana Achmad")  # Nama
+print(2605060052)  # NIM
+print("Magelang, 12 November 2005")  # ttl
+print("Tegalrejo, Magelang")  # alamat
+print("Laki-laki")  # jenis kelamin
+print("Sepak bola")  # hobi
